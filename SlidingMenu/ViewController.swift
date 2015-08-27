3095cafbcac9d07661a5b4e020622821f47f25e8
Nick Chen <nchen.dev@mac.com>
2015-08-26 23:56:26 -0700

    Add screen edge gesture recognizer
    Had to do this manually because of this bug that has been around since
    Xcode 6.4
    
    http://stackoverflow.com/questions/26256985/how-do-i-set-up-an-uiscreenedgepangesturerecognizer-using-interface-builder

--- SlidingMenu/ViewController.swift
+++ SlidingMenu/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var contentView: UIView!
+    var animator: UIDynamicAnimator!
+    
+    // You need to set up screen edge gesture recognizers programmatically
+    // There is a bug in interface builder
+    // http://stackoverflow.com/questions/26256985/how-do-i-set-up-an-uiscreenedgepangesturerecognizer-using-interface-builderhttp://stackoverflow.com/questions/26256985/how-do-i-set-up-an-uiscreenedgepangesturerecognizer-using-interface-builder
+    var leftScreenEdgeGestureRecognizer: UIScreenEdgePanGestureRecognizer!
+    var rightScreenEdgeGestureRecognizer: UIScreenEdgePanGestureRecognizer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.leftScreenEdgeGestureRecognizer = UIScreenEdgePanGestureRecognizer(target:self, action:"handleScreenEdgePan:")
+        self.leftScreenEdgeGestureRecognizer.edges = UIRectEdge.Left
+        self.leftScreenEdgeGestureRecognizer.delegate = self
+        self.view.addGestureRecognizer(self.leftScreenEdgeGestureRecognizer)
+
+        self.rightScreenEdgeGestureRecognizer = UIScreenEdgePanGestureRecognizer(target:self, action:"handleScreenEdgePan:")
+        self.rightScreenEdgeGestureRecognizer.edges = UIRectEdge.Right
+        self.rightScreenEdgeGestureRecognizer.delegate = self
+        self.view.addGestureRecognizer(self.rightScreenEdgeGestureRecognizer)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    func handleScreenEdgePan(recognizer: UIScreenEdgePanGestureRecognizer) {
+    }
 }
 
